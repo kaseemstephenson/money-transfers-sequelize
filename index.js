250c99bc1047f8e4420edab5faf1eb62306e6bbc
unknown <kstephenson@talentpath.com>
2022-01-12 14:56:32 -0500

    INit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.("Lets go");
+    res.send("Lets go");
 });
 
 app.listen(process.env.PORT || 4000, function(){
